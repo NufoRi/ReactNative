813ce54f4673ae1f54d4be6c7a73f1b4f101da87
Rijad Karadža <rijadkaradza@gmail.com>
2026-02-11 18:02:09 +0100

    first commit

---
+++ first app/App.js
@@ -0,0 +1,21 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './screens/MainScreen';
+import ExerciseScreen from './screens/ExerciseScreen';
+import TestScreen from './screens/TestScreen';
+import ListScreen from './screens/ListScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Home" component = {ListScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
